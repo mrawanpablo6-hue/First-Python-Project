cfa275c37235f111ae83e5cea4f0fefa58e6939c
Marwan <mrawanpablo6@gmail.com>
2026-06-07 18:26:53 +0300

    Remove test.py and pycache

--- test.py
+++
@@ -1,3 +0,0 @@
-total_second=int(input("Please type your second:\n"))
-minutes=total_second//60
-print(f"This course is:",{minutes//60},"hours and",{minutes%60},"minutes and",{total_second%60},"second")
